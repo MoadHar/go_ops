dcc9db007965713cf1f101b7bb7398dc224ed1c8
Mouad <m.harmach@gmail.com>
2024-05-16 01:38:53 +0200

    ...

--- 6.remote-data/gRPC/client/client.go
+++ 6.remote-data/gRPC/client/client.go
@@ -2,7 +2,7 @@ package client
 
 import (
 	"context"
-	pb "github.com/Moar/go_ops/remotedata/gRPC/proto"
+	pb "github.com/MoadHar/go_ops/6.remote-data/gRPC/proto"
 	"google.golang.org/grpc"
 	"time"
 )
---
+++ 6.remote-data/gRPC/go.mod
@@ -0,0 +1,3 @@
+module github.com/MoadHar/grpc/proto
+
+go 1.22.3
